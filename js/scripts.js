7af13546f70675f80240cc1d64e7e4d50d335706
Darrin Spell Jr <dspell74@gmail.com>
2019-09-22 22:46:15 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ const cards = document.querySelectorAll('.card');
 
 fetch('https://randomuser.me/api/')
     .then(response => response.json())
-    .then(data => generateCards(data.results["0"].picture.large))
+    .then(data => generateCards(data))
 
 //------------------------------------------
 //  HELPER FUNCTIONS
@@ -18,12 +18,12 @@ function generateCards(data) {
     const html = `
     <div class="card">
                     <div class="card-img-container">
-                        <img class="card-img" src="${data}" alt="profile picture">
+                        <img class="card-img" src="${data.results["0"].picture.large}" alt="profile picture">
                     </div>
                     <div class="card-info-container">
-                        <h3 id="name" class="card-name cap">first last</h3>
-                        <p class="card-text">email</p>
-                        <p class="card-text cap">city, state</p>
+                        <h3 id="name" class="card-name cap">${data.results["0"].name.first} ${data.results["0"].name.last}</h3>
+                        <p class="card-text">${data.results["0"].email}</p>
+                        <p class="card-text cap">${data.results["0"].location.city}</p>
                     </div>
                 </div>
     `;
