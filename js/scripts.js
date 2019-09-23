b34f0aad6f511f5659d71ba6b7aeeeb581dd9392
Darrin Spell Jr <dspell74@gmail.com>
2019-09-22 23:34:11 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -21,9 +21,9 @@ function generateCards(data) {
                         <img class="card-img" src="${data.results["0"].picture.large}" alt="profile picture">
                     </div>
                     <div class="card-info-container">
-                        <h3 id="name" class="card-name cap">Name: ${data.results["0"].name.first} ${data.results["0"].name.last}</h3>
-                        <p class="card-text">Email: ${data.results["0"].email}</p>
-                        <p class="card-text cap">Location: ${data.results["0"].location.city}</p>
+                        <h3 id="name" class="card-name cap">${data.results["0"].name.first} ${data.results["0"].name.last}</h3>
+                        <p class="card-text">${data.results["0"].email}</p>
+                        <p class="card-text cap">${data.results["0"].location.city}</p>
                     </div>
                 </div>
     `;
