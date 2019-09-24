6a063627c31795008f27770692fb897104f3d8c6
Darrin Spell Jr <dspell74@gmail.com>
2019-09-23 21:04:39 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,39 @@
 const searchContainer = document.querySelector('.search-container');
 const gallery = document.getElementById('gallery');
-const cards = document.querySelectorAll('.card');
+const card = document.querySelectorAll('.card');
+const modal = document.querySelectorAll('.modal')
 
 //------------------------------------------
 //  FETCH FUNCTIONS 
 //------------------------------------------
+function fetchData(url) {
+    return fetch(url)
+        .then(response => response.json())
+}
 
-fetch('https://randomuser.me/api/')
-    .then(response => response.json())
-    .then(data => generateCards(data))
+fetchData('https://randomuser.me/api/?results=12')
+    .then(data => generateCards(data.results))
 
 //------------------------------------------
 //  HELPER FUNCTIONS
 //------------------------------------------
 
 function generateCards(data) {
-    const html = `
-    <div class="card">
+    const card = data.map(card => `
+     <div class="card">
                     <div class="card-img-container">
-                        <img class="card-img" src="${data.results["0"].picture.large}" alt="profile picture">
-                    </div>
+                        <img class="card-img" src="${card.picture.large}" alt="profile picture">
+                     </div>
                     <div class="card-info-container">
-                        <h3 id="name" class="card-name cap">${data.results["0"].name.first} ${data.results["0"].name.last}</h3>
-                        <p class="card-text">${data.results["0"].email}</p>
-                        <p class="card-text cap">${data.results["0"].location.city}</p>
-                    </div>
+                        <h3 id="name" class="card-name cap" value=${data.results}>${card.name.first} ${card.name.last}</h3>
+                        <p class="card-text">${card.email}</p>
+                        <p class="card-text cap">${card.location.city}</p>
+                     </div>
                 </div>
-    `;
-    gallery.innerHTML = html;
+    `).join('');
+    gallery.innerHTML = card;
 }
+
+//------------------------------------------
+//  EVENT LISTENERS
+//------------------------------------------
