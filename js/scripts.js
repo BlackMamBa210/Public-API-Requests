72cdbd4ec6c7797316925f091294a0c6383ca5a8
Darrin Spell Jr <dspell74@gmail.com>
2019-09-26 09:27:35 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 const searchContainer = document.querySelector('.search-container');
 const gallery = document.getElementById('gallery');
-const card = document.querySelectorAll('.card');
 const modal = document.querySelectorAll('.modal');
 
 //------------------------------------------
@@ -31,12 +30,37 @@ function generateCards(data) {
                      </div>
                 </div>
     `).join('');
+
     gallery.innerHTML = card;
+    console.log(data);
+    return data;
 }
 
-function generateModal(data) {
-    const modalContainer = document.querySelectorAll('.card');
+function generateModal() {
+    const card = document.querySelectorAll('.card')
+    const modalContainer = card.querySelector('.modal-container');
+    const modalInfoContainer = card.querySelector('.modal-info-container');
+    const body = document.querySelector('body');
 
+    card.addEventListener('click', () => {
+        body.append(`
+        <div class="modal-container">
+                <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                    <div class="modal-info-container">
+                        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">name</h3>
+                        <p class="modal-text">email</p>
+                        <p class="modal-text cap">city</p>
+                        <hr>
+                        <p class="modal-text">(555) 555-5555</p>
+                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                        <p class="modal-text">Birthday: 10/21/2015</p>
+                    </div>
+                </div>
+        `)
+    })
+    console.log('body')
 }
 //------------------------------------------
 //  EVENT LISTENERS
